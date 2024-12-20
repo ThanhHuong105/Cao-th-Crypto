ed8824efacd98de82d9a2e7326c6988570c9783b
ThanhHuong105 <nexagennews@gmail.com>
2024-12-20 17:29:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
     Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 )
-
 from apscheduler.jobstores.base import ConflictingIdError
 
 # Bot Constants
